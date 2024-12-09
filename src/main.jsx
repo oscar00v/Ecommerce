6cd6f99bc9555dc1bbf396febbada63e14b7b998
Oscar00v <Oscar00v@gmail.com>
2024-12-09 12:39:09 -0600

    Boostrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 // import './index.css'
 // import App from './App.jsx'
 // import App2 from './App2.jsx'
