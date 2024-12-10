2c275d4a41c7fe12f087691d2630e832257d615c
Oscar00v <Oscar00v@gmail.com>
2024-12-10 17:11:11 -0600

    Cambios en Navbar al hacer Login

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuth'; // Ajusta la ruta si es necesario
 import './Navbar.scss';
 
 function Navbar() {
+  const { isAuth, logout } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(); // Llama a la función logout del contexto
+    navigate('/'); // Redirige al usuario al home o login
+  };
+
   return (
     <nav className='header'>
       <NavLink to="/" className='header__logo'>
@@ -34,37 +43,135 @@ function Navbar() {
             Departamento
           </NavLink>
         </li>
-        <li className='header__list-item'>
-          <NavLink 
-            to="/singup" 
-            className={({ isActive }) => 
-              isActive 
-                ? 'header__item-link header__item-link--is-active' 
-                : 'header__item-link'
-            }
-          >
-            Singup
-          </NavLink>
-        </li>
 
-        <li className='header__list-item'>
-          <NavLink 
-            to="/login" 
-            className={({ isActive }) => 
-              isActive 
-                ? 'header__item-link header__item-link--is-active' 
-                : 'header__item-link'
-            }
-          >
-            Login
-          </NavLink>
-        </li>
+        {!isAuth ? (
+          // Mostrar estos enlaces si NO está autenticado
+          <>
+            <li className='header__list-item'>
+              <NavLink 
+                to="/singup" 
+                className={({ isActive }) => 
+                  isActive 
+                    ? 'header__item-link header__item-link--is-active' 
+                    : 'header__item-link'
+                }
+              >
+                Signup
+              </NavLink>
+            </li>
+            <li className='header__list-item'>
+              <NavLink 
+                to="/login" 
+                className={({ isActive }) => 
+                  isActive 
+                    ? 'header__item-link header__item-link--is-active' 
+                    : 'header__item-link'
+                }
+              >
+                Login
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          // Mostrar este enlace si está autenticado
+          <>
+          <li className='header__list-item'>
+              <NavLink 
+                to="/dashboard" 
+                className={({ isActive }) => 
+                  isActive 
+                    ? 'header__item-link header__item-link--is-active' 
+                    : 'header__item-link'
+                }
+              >
+                Dashboard
+              </NavLink>
+            </li> 
+            <li className='header__list-item'>
+              <button 
+                onClick={handleLogout} 
+                className='header__item-link header__logout-btn'
+              >
+                {}Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
+
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import './Navbar.scss';
+
+// function Navbar() {
+//   return (
+//     <nav className='header'>
+//       <NavLink to="/" className='header__logo'>
+//         LOGO
+//       </NavLink>
+
+//       <ul className='header__nav-list'>
+//         <li className='header__list-item'>
+//           <NavLink 
+//             to="/" 
+//             className={({ isActive }) => 
+//               isActive 
+//                 ? 'header__item-link header__item-link--is-active' 
+//                 : 'header__item-link'
+//             }
+//           >
+//             Home
+//           </NavLink>
+//         </li>
+//         <li className='header__list-item'>
+//           <NavLink 
+//             to="/departamento" 
+//             className={({ isActive }) => 
+//               isActive 
+//                 ? 'header__item-link header__item-link--is-active' 
+//                 : 'header__item-link'
+//             }
+//           >
+//             Departamento
+//           </NavLink>
+//         </li>
+//         <li className='header__list-item'>
+//           <NavLink 
+//             to="/singup" 
+//             className={({ isActive }) => 
+//               isActive 
+//                 ? 'header__item-link header__item-link--is-active' 
+//                 : 'header__item-link'
+//             }
+//           >
+//             Singup
+//           </NavLink>
+//         </li>
+
+//         <li className='header__list-item'>
+//           <NavLink 
+//             to="/login" 
+//             className={({ isActive }) => 
+//               isActive 
+//                 ? 'header__item-link header__item-link--is-active' 
+//                 : 'header__item-link'
+//             }
+//           >
+//             Login
+//           </NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
 
 
 
